c2f38e3b6343cb2c73f95d21bb0325695999281e
uoa-kcho598 <kcho598@aucklanduni.ac.nz>
2020-08-31 20:00:44 +1200

    Update main.c

--- FInalImplementation/Software/Mid_Sem_Review/Mid_Sem_Review/main.c
+++ FInalImplementation/Software/Mid_Sem_Review/Mid_Sem_Review/main.c
@@ -17,12 +17,12 @@
 
 int main(void)
 {
-	usart_init(BAUDRATE);
+	usart_init(BAUDRATE); //initialises uart with baudrate of BAUDRATE value
     while (1) {
-		usart_transmit_voltage(RMSVoltage);
-		usart_transmit_current(PeakCurrent);
-		usart_transmit_power(Power);
-		_delay_ms(1000);
+		usart_transmit_voltage(RMSVoltage); //calls function to transmit RMSVoltage value
+		usart_transmit_current(PeakCurrent); //calls function to transmit PeakCurrent value
+		usart_transmit_power(Power); //calls function to transmit Power value
+		_delay_ms(1000); //sets a delay for 1 second 
 
 	}
 }
