6473e8cb92a7304f2c75411cf1740ace438c96f9
uoa-kcho598 <kcho598@aucklanduni.ac.nz>
2020-08-31 19:08:55 +1200

    Shifted code to final software implementation

---
+++ FInalImplementation/Software/Mid_Sem_Review/Mid_Sem_Review/common.h
@@ -0,0 +1,17 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 28/08/2020 6:32:03 PM
+ *  Author: krish
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+#define BAUDRATE 9600
+#define RMSVoltage 999
+#define PeakCurrent 50
+#define Power 999
+
+#endif /* INCFILE1_H_ */
